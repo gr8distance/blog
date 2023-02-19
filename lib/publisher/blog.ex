7dde79f176d565e6c10e8bd737f2de907d703891
ug <gr8.distance@gmail.com>
2023-02-19 13:26:07 +0900

    ブログ記事をロードするための処理を追加

---
+++ lib/publisher/blog.ex
@@ -0,0 +1,20 @@
+defmodule Publisher.Blog do
+  alias Publisher.Blog.Post
+
+  use NimblePublisher,
+    build: Post,
+    from: Application.app_dir(:publisher, "priv/posts/**/*.md"),
+    as: :posts,
+    highlighters: [:makeup_elixir, :makeup_erlang]
+
+  # The @posts variable is first defined by NimblePublisher.
+  # Let's further modify it by sorting all posts by descending date.
+  @posts Enum.sort_by(@posts, & &1.date, {:desc, Date})
+
+  # Let's also get all tags
+  @tags @posts |> Enum.flat_map(& &1.tags) |> Enum.uniq() |> Enum.sort()
+
+  # And finally export them
+  def all_posts, do: @posts
+  def all_tags, do: @tags
+end
---
+++ lib/publisher/blog/post.ex
@@ -0,0 +1,13 @@
+defmodule Publisher.Blog.Post do
+  @enforce_keys [:id, :author, :title, :body, :description, :tags, :date]
+  defstruct [:id, :author, :title, :body, :description, :tags, :date]
+
+  require IEx
+
+  def build(filename, attrs, body) do
+    [year, month_day_id] = filename |> Path.rootname() |> Path.split() |> Enum.take(-2)
+    [month, day, id] = String.split(month_day_id, "-", parts: 3)
+    date = Date.from_iso8601!("#{year}-#{month}-#{day}")
+    struct!(__MODULE__, [id: id, date: date, body: body] ++ Map.to_list(attrs))
+  end
+end

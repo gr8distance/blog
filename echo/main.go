96a95bb9970ea1945cbd22d572bd8939ad0e6e7e
ug <gr8.distance@gmail.com>
2023-11-18 00:33:19 +0900

    JSONをパースする

--- echo/main.go
+++ echo/main.go
@@ -1,16 +1,67 @@
 package main
 
 import (
-	"app/api"
+	"encoding/json"
 	"fmt"
+	"log"
 
-	"github.com/labstack/echo/v4"
+	"io/ioutil"
+	"net/http"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-func main() {
-	fmt.Println("Hello World")
-	e := echo.New()
+func connect(dbName string) (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open(dbName), &gorm.Config{})
+	return db, err
+}
 
-	api.DefineRoutes(e)
-	e.Logger.Fatal(e.Start(":8000"))
+type Post struct {
+	Id          string   `json:"id"`
+	Title       string   `json:"title"`
+	Author      string   `json:"author"`
+	Body        string   `json:"body"`
+	Date        string   `json:"date"`
+	Description string   `json:"description"`
+	HeaderImage string   `json:"header_image"`
+	Keywords    []string `json:"keywords"`
+	Tags        []string `json:"tags"`
+	Year        string   `json:"year"`
+	Month       string   `json:"month"`
+	Day         string   `json:"day"`
+}
+
+type Response struct {
+	Posts []Post `json:"posts"`
+}
+
+func fetch(url string) []byte {
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	return bytes
+}
+
+func main() {
+	url := "http://localhost:4000/api/blogs/all"
+	bytes := fetch(url)
+	var response Response
+	if err := json.Unmarshal(bytes, &response); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(response)
+
+	// fmt.Println(len(response.posts))
+	// fmt.Println(string(byteArray))
+
+	// db, err := connect("blog.db")
+	// if err != nil {
+	//  	panic("failed to connect database")
+	// }
+
+	// e := echo.New()
+	//
+	// api.DefineRoutes(e)
+	// e.Logger.Fatal(e.Start(":8000"))
 }

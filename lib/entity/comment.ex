76be7d336254769d9cebcca5d8fcdf95008a0eb4
ug <gr8.distance@gmail.com>
2024-05-28 08:54:24 +0900

    fxup

--- lib/entity/comment.ex
+++ lib/entity/comment.ex
@@ -2,9 +2,9 @@ defmodule Entity.Comment do
   @type t :: %__MODULE__{
           id: String.t(),
           post_id: String.t(),
-          content: String.t(),
+          body: String.t(),
           author: String.t(),
           commented_at: DateTime.t()
         }
-  defstruct id: "", post_id: "", content: "", author: "", commented_at: nil
+  defstruct id: "", post_id: "", body: "", author: "", commented_at: nil
 end
--- lib/entity/post.ex
+++ lib/entity/post.ex
@@ -4,9 +4,9 @@ defmodule Entity.Post do
   @type t :: %__MODULE__{
           id: String.t(),
           title: String.t(),
-          content: String.t(),
+          body: String.t(),
           published_at: DateTime.t(),
           comments: [Comment.t()]
         }
-  defstruct id: "", title: "", content: "", published_at: nil, comments: []
+  defstruct id: "", title: "", body: "", published_at: nil, comments: []
 end

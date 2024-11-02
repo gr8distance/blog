ad815ca1e576b4ace315d0c65fb8666b989f6784
ug <gr8.distance@gmail.com>
2024-11-02 22:27:46 +0900

    ruby scripts

---
+++ scripts/article.rb
@@ -0,0 +1,30 @@
+require 'pry'
+require 'fileutils'
+require 'parallel'
+
+Dir.glob('src/pages/articles/**/**/**/*.astro') do |path|
+  year, month, day, title = path.gsub('.astro', '').split('/').drop(3)
+  file = File.read(path)
+  formatted = file
+                .gsub('>', ">\n")
+                .gsub("\n\n", "\n")
+
+  hrefs = formatted
+            .split("\n")
+            .map { |line| line.match(/src="https:\/\/blogger.googleusercontent.com.*"/) }
+            .compact
+  images = Dir.glob("public/images/#{year}/#{month}/#{day}/#{title.gsub(' ', '_')}/*").to_a
+
+  body = hrefs.zip(images).reduce(formatted) do |acm, (href, image)|
+    if href.nil? || image.nil?
+      acm
+    else
+      acm.gsub(href.to_s.split("\"")[1], image)
+    end
+  end
+
+  File.open(path, 'r+') do |f|
+    f.puts body
+    f.puts
+  end
+end
